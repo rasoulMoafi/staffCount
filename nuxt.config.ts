bae44e47d0bf5d6b3e643c8e4fe82d5e102e0bfe
rasoul <rasoulhk8@gmail.com>
2026-06-21 12:13:37 +0330

    Setup GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
 import tailwindcss from '@tailwindcss/vite'
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+
   devtools: { enabled: true },
+
   app: {
-    baseURL: process.env.NUXT_APP_BASE_URL || '/',
+    baseURL: '/my-nuxt-app/',
   },
+
   css: ['~/assets/css/main.css'],
+
   vite: {
     plugins: [tailwindcss()],
+  },
+
+  nitro: {
+    preset: 'github_pages',
   },
 })
